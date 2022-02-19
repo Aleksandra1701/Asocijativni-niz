2d640d08609e4ed7b994baeac5403c8bf91f76ed
Aleksandra <60824427+Aleksandra1701@users.noreply.github.com>
2022-02-19 16:47:27 +0100

    Add files via upload

---
+++ Asocijativni niz.php
@@ -0,0 +1,7 @@
+<?php
+$myInfo = array("Alex", 29, "Smederevo", "College");
+echo "My name is" . $myInfo[0] . "\n";
+echo "I am" . $myInfo[1] . "year old\n";
+echo "I live in" . $myInfo[2] . "\n";
+echo "My latest education level is" . $myInfo[3];
+ ?>
